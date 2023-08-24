cb03aa83a3590a5e6481c5d7ed0bb74c41bf2bd1
unknown <jamccabe19@gmail.com>
2023-08-24 11:14:26 -0700

    Only allows for binary classification. Only predicts one class for some reason.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import argparse as ap
+import sklearn.metrics
 
 class DenseLayer:
     def __init__(self, neurons):
@@ -12,6 +13,7 @@ class DenseLayer:
         return 1 / (1 + np.exp(-Z))
 
     def _softmax(self, Z):
+        
         exp = np.exp(Z)
         exp_sum = np.sum(exp, axis=1, keepdims=True)
         return exp/exp_sum
@@ -28,7 +30,6 @@ class DenseLayer:
     def forward(self, A_prev, W_curr, b_curr, activation):
         ''' Forward Propagation of a single layer '''
         Z_curr = np.dot(A_prev, W_curr.T) + b_curr
-
         if activation == "relu":
             A_curr = self._relu(Z_curr)
         elif activation == "sigmoid":
@@ -42,14 +43,20 @@ class DenseLayer:
 
     def backward(self, dA_curr, W_curr, Z_curr, A_prev, activation):
         ''' Backward Propagation of a single layer '''
+        m = A_prev.shape[0]
+
         if activation == 'softmax':
-            dW_curr = np.dot(A_prev.T, dA_curr)
-            db_curr = np.sum(dA_curr, axis=0, keepdims=True)
+            dW_curr = (1/m) * np.dot(A_prev.T, dA_curr)
+            db_curr = (1/m) * np.sum(dA_curr, axis=0, keepdims=True)
+            dA_prev = np.dot(dA_curr, W_curr)
+        elif activation == 'sigmoid':
+            dW_curr = (1/m) * np.dot(A_prev.T, dA_curr)
+            db_curr = (1/m) * np.sum(dA_curr, axis=0, keepdims=True)
             dA_prev = np.dot(dA_curr, W_curr)
         elif activation == 'relu':
             dZ_curr = self._derivative_relu(dA_curr, Z_curr)
-            dW_curr = np.dot(A_prev.T, dZ_curr)
-            db_curr = np.sum(dZ_curr, axis=0, keepdims=True)
+            dW_curr = (1/m) * np.dot(A_prev.T, dZ_curr)
+            db_curr = (1/m) * np.sum(dZ_curr, axis=0, keepdims=True)
             dA_prev = np.dot(dZ_curr, W_curr)
         else:
             raise Exception('Activation function '+activation+' not supported')
@@ -100,16 +107,7 @@ class Network:
             self.params.append({
                 'W':np.random.uniform(-init_range, init_range, (output_size, input_size)),
                 'b':np.random.uniform(-init_range, init_range, (1, output_size))
-            })
-            #self.memory.append({
-            #    'Z':np.ones((num_data, output_size)),
-            #    'A':np.ones((num_data, input_size))
-            #})
-            #self.gradients.append({
-            #    'dW':np.ones((output_size, input_size)),
-            #    'db':np.ones((1, output_size))
-            #})
-            
+            })    
 
     def _forward_propagation(self, X):
         ''' Does a forward pass through full NN '''
@@ -132,41 +130,61 @@ class Network:
     def _backward_propagation(self, y_hat, y):
         ''' Does a backward pass through full NN '''
         #y = y.reshape(y_hat.shape)
-        #COMPUTE LOSS: Only giong to do categorical cross-entropy
-        #              for multi-classification first
-        #dA_prev = -(np.divide(y, y_hat) - np.divide(1-y, 1-y_hat))
-        dA_prev = y_hat - 1
-        dA_prev /= len(y)
+        #COMPUTE LOSS: Using simplification. So it only works
+        # if you use one of the following pairs:
+        #   Output activation   | Loss Function
+        # --------------------------------------
+        #       identity        | Squared Error
+        #   logistic sigmoid    | Binary Cross Entropy
+        #       softmax         | Multiclass Cross Entropy
+        dA_prev = y_hat - y
 
         for idx, layer in reversed(list(enumerate(self.network))):
-            #mem_idx = -(idx +1)
-            #print('idx:',idx,'len(memory)',len(self.memory))
             dA_curr = dA_prev
-            A_prev = self.memory[idx]['A']
-            Z_curr = self.memory[idx]['Z']
+            A_prev = self.memory[idx-len(self.network)]['A']
+            Z_curr = self.memory[idx-len(self.network)]['Z']
             W_curr = self.params[idx]['W']
             activation = self.architecture[idx]['activation']
 
             dA_prev, dW_curr, db_curr = layer.backward(
                 dA_curr, W_curr, Z_curr, A_prev, activation)
             
-            #self.gradients[idx] = {'dW':dW_curr , 'db':db_curr}
             self.gradients.append({'dW':dW_curr, 'db':db_curr})
 
     def _gradient_descent(self, learn_rate):
         for idx, layer in enumerate(self.network):
-            self.params[idx]['W'] -= learn_rate * list(reversed(self.gradients))[idx]['dW'].T
-            self.params[idx]['b'] -= learn_rate * list(reversed(self.gradients))[idx]['db']
+            self.params[idx]['W'] -= learn_rate * list(reversed(self.gradients))[idx-len(self.network)]['dW'].T
+            self.params[idx]['b'] -= learn_rate * list(reversed(self.gradients))[idx-len(self.network)]['db']
 
     def _get_accuracy(self, y_hat, y):
-        return np.mean(np.argmax(y_hat, axis=1) == y)
-        
+        ''' y_hat and y are onehot'''
+        pred = np.argmax(y_hat, axis=1)
+        pred = self._one_hot_encode(pred)
+        #print(pred.shape, y_hat.shape, y.shape)
+        #acc = np.sum(np.where(pred == y, 1, 0), axis=0)
+        #print(np.array(pred==y).shape)
+        return np.mean(pred == y)
+
     def _calculate_loss(self, y_hat, y):
         ''' Cross Entropy Loss '''
-        eps = 1e-5
-        log_prob = -np.log(y_hat + eps)
-        return np.sum(log_prob)/len(y)
+        #eps = 1e-5
+        #log_prob = -np.log(y_hat + eps)
+        #return np.sum(log_prob)/len(y)
+        #BINARY CROSS ENTROPY RIGHT NOW
+        logits = np.multiply(np.log(y_hat), y) + np.multiply((1 - y), np.log(1 - y_hat))
+        return - (np.sum(logits) / len(y))
 
+    def _one_hot_encode(self, y):
+        y = y.astype(int)
+        num_classes = self.architecture[-1]['output_dim']
+        return np.eye(num_classes)[y]
+
+    def norm(self, X, x_min, x_max):
+        nom = (X-X.min(axis=0))*(x_max-x_min)
+        denom = X.max(axis=0) - X.min(axis=0)
+        denom[denom==0] = 1
+        return x_min + nom/denom 
+    
     def fit(self, X, y, init_range, epochs, learn_rate):
         ''' Train the NN 
         TODO - Add in minibatching
@@ -176,50 +194,52 @@ class Network:
              - Be able to predict on test set
         '''
         self._init_layers(X.shape[1], init_range)
-        
+        X = self.norm(X,0,1)
+        y = self._one_hot_encode(y)
+
         self.loss = []
-        self.dev_loss = []
-        self.accuracy = []
+        #self.accuracy = []
+
         while self.curr_epoch <= epochs:
             y_hat = self._forward_propagation(X)
 
             self.loss.append(self._calculate_loss(y_hat, y))
-            self.accuracy.append(self._get_accuracy(y_hat, y))
+            #self.accuracy.append(self._get_accuracy(y, y_hat))
 
             self._backward_propagation(y_hat, y)
             self._gradient_descent(learn_rate)
-
         
-            if self.curr_epoch % 100 == 0:
-                s = 'EPOCH: {}, ACCURACY: {}, LOSS: {}'.format(self.curr_epoch, self.accuracy[-1], self.loss[-1])
+            if self.curr_epoch % 500 == 0:
+                s = 'EPOCH: {}, LOSS: {}'.format(self.curr_epoch, self.loss[-1])
                 print(s)
 
             self.curr_epoch += 1
+    
+    def evaluate(self, X, y):
+        y = self._one_hot_encode(y)
+        y_hat = self._forward_propagation(X)
+        #preds = y_hat > 0.5
+        preds = np.rint(y_hat)
+        #print(preds.shape, y.shape)
+        #print(np.concatenate((preds,y),axis=1))
+        return np.mean(preds == y)
+
             
 
-
-train_X = np.loadtxt("./data/dataset2.train_features.txt", dtype=float).astype(np.float32)
-train_y = np.loadtxt("./data/dataset2.train_targets.txt", dtype=float).astype(np.float32)
-dev_X   = np.loadtxt("./data/dataset2.dev_features.txt", dtype=float).astype(np.float32)
-dev_y   = np.loadtxt("./data/dataset2.dev_targets.txt", dtype=float).astype(np.float32)
+train_X = np.loadtxt("./data/dataset9.train_features.txt", dtype=float).astype(np.float32)
+train_y = np.loadtxt("./data/dataset9.train_targets.txt", dtype=float).astype(np.float32)
+dev_X   = np.loadtxt("./data/dataset9.dev_features.txt", dtype=float).astype(np.float32)
+dev_y   = np.loadtxt("./data/dataset9.dev_targets.txt", dtype=float).astype(np.float32)
 
 model = Network()
-model.add(DenseLayer(4))
-#model.add(DenseLayer(6))
-#model.add(DenseLayer(8))
-#model.add(DenseLayer(4))
-model.add(DenseLayer(3))
+model.add(DenseLayer(50))
+model.add(DenseLayer(50))
+model.add(DenseLayer(50))
+model.add(DenseLayer(25))
+model.add(DenseLayer(2))
 model.compile(train_X)
-#model._init_layers(1)
-model.fit(train_X, train_y, 0.4, 50, 0.1)
-print('gradients length: ', len(model.gradients))
-print('params length: ', len(model.params))
-print('memory length: ', len(model.memory))
-print(train_X.shape)
-print('W1',model.params[0]['W'].shape, ' b1', model.params[0]['b'].shape)
-print('Z1', model.memory[0]['Z'].shape,' A1', model.memory[0]['A'].shape)
-print('dW1',model.gradients[1]['dW'].shape, ' db1', model.gradients[1]['db'].shape)
-print()
-print('W2',model.params[1]['W'].shape, ' b2', model.params[1]['b'].shape)
-print('Z2',model.memory[1]['Z'].shape, ' A2',model.memory[1]['A'].shape)
-print('dW2',model.gradients[0]['dW'].shape, ' db2', model.gradients[0]['db'].shape)
+model.fit(train_X, train_y, 0.01, 1, 0.001)
+
+accuracy = model.evaluate(dev_X, dev_y)
+print(accuracy)
+#print(dev_y)
